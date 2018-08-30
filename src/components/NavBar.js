65fc470c9ec7a4c41a15e03b4ea0ae022b73587e
Daniel Rosato <rosato.daniel@gmail.com>
2018-08-30 20:16:40 +0200

    minor fixes + not found page

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -5,15 +5,12 @@ class NavBar extends Component {
   render() {
     return (
       <nav className="navbar navbar-expand-lg navbar-light bg-light">
-        <Link className="navbar-brand" to="/"> Home </Link>
+        <Link className="navbar-brand" to="/chairs"> Chairs </Link>
         <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
           <span className="navbar-toggler-icon"></span>
         </button>
         <div className="collapse navbar-collapse" id="navbarSupportedContent">
           <ul className="navbar-nav mr-auto">
-            <li className="nav-item active">
-              <Link className="nav-item nav-link active" to="/chairs"> Chairs </Link>
-            </li>
             <li className="nav-item">
               <Link className="nav-item nav-link active" to="/beacons"> Beacons </Link>
             </li>
--- src/routes.js
+++ src/routes.js
@@ -1,6 +1,7 @@
 import React from 'react';
-import { Router, Route } from 'react-router-dom';
+import { Router, Route, Switch } from 'react-router-dom';
 import App from './components/App';
+import NotFound from './components/NotFound';
 import BeaconListContainer from './containers/beacons/BeaconListContainer';
 import BeaconNewContainer from './containers/beacons/BeaconNewContainer';
 import BeaconEditContainer from './containers/beacons/BeaconEditContainer';
@@ -24,7 +25,6 @@ import PropsRoute from "./components/PropsRoute";
 const history = createHistory();
 const location = history.location;
 const unlisten = history.listen((location, action) => {
-  console.log("listening");
   store.dispatch(setRedirectFalse());
   store.dispatch(removeMessage());
 })
@@ -33,35 +33,42 @@ export default () => {
   return (
     <Router history={history}>
       <App>
-        <Route exact path='/beacons/new' render={ () => {
-              store.dispatch(getChairsRequest());
-              return <BeaconNewContainer />
-        }}/>
-        <Route exact path='/beacons/:id/edit' render={ (props) => {
-              store.dispatch(getBeaconRequest(props.match.params.id));
-              store.dispatch(getChairsRequest());
-              return <BeaconEditContainer />
-        }}/>
-        <Route exact path='/beacons' render={ () => {
-              store.dispatch(getBeaconsRequest());
-              return <BeaconListContainer />
-        }}/>
-        <Route path='/chairs/new' component={ChairNewContainer}/>
-        <Route exact path='/chairs/:id/edit' render={ (props) => {
-              store.dispatch(getChairRequest(props.match.params.id));
-              return <ChairEditContainer />
-        }}/>
-        <Route exact path='/chairs/:id/panel' render={ (props) => {
-              store.dispatch(getChairRequest(props.match.params.id));
-              store.dispatch(getChairDataRequest(props.match.params.id, 200));
-              store.dispatch(getBeaconDataFromChairRequest(props.match.params.id, 200));
-              store.dispatch(getKMeansDataRequest(props.match.params.id, 200));
-              return <ChairPanelContainer chairId={props.match.params.id} />
-        }}/>
-        <Route exact path='/chairs' render={ () => {
-              store.dispatch(getChairsRequest());
-              return <ChairListContainer />
-        }}/>
+        <Switch>
+          <Route exact path='/' render={ () => {
+                store.dispatch(getChairsRequest());
+                return <ChairListContainer />
+          }}/>
+          <Route exact path='/beacons/new' render={ () => {
+                store.dispatch(getChairsRequest());
+                return <BeaconNewContainer />
+          }}/>
+          <Route exact path='/beacons/:id/edit' render={ (props) => {
+                store.dispatch(getBeaconRequest(props.match.params.id));
+                store.dispatch(getChairsRequest());
+                return <BeaconEditContainer />
+          }}/>
+          <Route exact path='/beacons' render={ () => {
+                store.dispatch(getBeaconsRequest());
+                return <BeaconListContainer />
+          }}/>
+          <Route path='/chairs/new' component={ChairNewContainer}/>
+          <Route exact path='/chairs/:id/edit' render={ (props) => {
+                store.dispatch(getChairRequest(props.match.params.id));
+                return <ChairEditContainer />
+          }}/>
+          <Route exact path='/chairs/:id/panel' render={ (props) => {
+                store.dispatch(getChairRequest(props.match.params.id));
+                store.dispatch(getChairDataRequest(props.match.params.id, 200));
+                store.dispatch(getBeaconDataFromChairRequest(props.match.params.id, 200));
+                store.dispatch(getKMeansDataRequest(props.match.params.id, 200));
+                return <ChairPanelContainer chairId={props.match.params.id} />
+          }}/>
+          <Route exact path='/chairs' render={ () => {
+                store.dispatch(getChairsRequest());
+                return <ChairListContainer />
+          }}/>
+          <Route component={NotFound} />
+        </Switch>
       </App>
     </Router>
   )
