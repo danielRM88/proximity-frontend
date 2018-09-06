bcb8f92381d000a80274bf60fa5c85f467c7cd16
Daniel Rosato <rosato.daniel@gmail.com>
2018-09-06 22:46:00 +0200

    fix for nav bar

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -6,13 +6,14 @@ class NavBar extends Component {
     return (
       <nav className="navbar navbar-expand-lg navbar-light bg-light">
         <Link className="navbar-brand" to="/chairs"> Chairs </Link>
-        <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+        <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
           <span className="navbar-toggler-icon"></span>
         </button>
+
         <div className="collapse navbar-collapse" id="navbarSupportedContent">
           <ul className="navbar-nav mr-auto">
-            <li className="nav-item">
-              <Link className="nav-item nav-link active" to="/beacons"> Beacons </Link>
+            <li className="nav-item active">
+              <Link className="nav-link" to="/beacons"> Beacons </Link>
             </li>
           </ul>
         </div>
