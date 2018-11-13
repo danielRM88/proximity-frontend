9139f9f11d9e43de88d0c5aadd28822a0ece24b9
Daniel Rosato <rosato.daniel@gmail.com>
2018-11-13 15:50:16 +0100

    removed continuous adjustment for now

--- src/components/chairs/ChairFilterForm.js
+++ src/components/chairs/ChairFilterForm.js
@@ -17,14 +17,14 @@ class ChairFilterForm extends Component {
           <label className="required" htmlFor="processNoise">Filter's Process Noise (V1)</label>
           <input type="number" step="any" name="processNoise" className="form-control" defaultValue={processNoise} onChange={(event) => {this.handleProcessNoiseChange(event, this)}} autoFocus required/>
         </div>
-        <div className="form-group">
+        {/*<div className="form-group">
           <label htmlFor="continuousAdjustment">Continuous Adjustment</label>
           <input type="checkbox" name="continuousAdjustment" className="form-control" defaultChecked={continuousAdjustment} onChange={(event) => {this.handleAdjustmentChange(event, this)}} />
         </div>
         <div className="form-group">
           <label className="required" htmlFor="adjustmentThreshold">Adjustment Threshold</label>
           <input type="number" step="any" name="adjustmentThreshold" className="form-control" defaultValue={adjustmentThreshold} onChange={(event) => {this.handleAdjustmentThresholdChange(event, this)}} autoFocus required/>
-        </div>
+        </div>*/}
         <div className="text-center">
           <button type="submit" className="btn btn-primary"> Update Filter </button>
         </div>
